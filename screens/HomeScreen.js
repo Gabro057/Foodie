da4cf9e685917131f3a2c5fbd67bcfdd791d8d36
Gabro057 <gabro057@seznam.cz>
2022-07-30 21:38:12 +0200

    Featured rows with Restaurant cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 import { AdjustmentsIcon, ChevronDownIcon, UserIcon, SearchIcon } from "react-native-heroicons/outline";
 // Components
 import Categories from './components/Categories';
+import FeaturedRow from './components/FeaturedRow';
 
 const HomeScreen = () => {
   const basicClr = "#00ccbb"
@@ -17,8 +18,7 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <SafeAreaView className="bg-white pt-5">
-      <Text>
+    <SafeAreaView className="bg-white pt-5">      
         {/* HEADER */}
         <View className="flex flex-row pb-3 items-center mx-4 space-x-2 px-4">
           <Image source={{ uri: 'https://links.papareact.com/wru' }} className='h-7 w-7 bg-gray-300 p-4 rounded-full' />
@@ -34,7 +34,7 @@ const HomeScreen = () => {
           <UserIcon size={35} color={basicClr} />
         </View>
         {/* SEARCH */}
-        <View className="flex flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex flex-row items-center space-x-2 pb-2 mx-4">
           <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
             <SearchIcon color='gray' size={20}/>
             <TextInput placeholder="Restaurants and cuisines" keyboardType='default' />
@@ -43,13 +43,16 @@ const HomeScreen = () => {
         </View>
 
         {/* BODY */}
-        <ScrollView>
+        <ScrollView className="bg-gray-100" contentContainerStyle={{ paddingBottom: 100 }}>
           {/* Categories */}
           <Categories />
           {/* Fetured rows */}
-
-        </ScrollView>
-      </Text>
+          <FeaturedRow id="123" title="Featured" description="Paid placements from our partners" featuredCategory="featured"/>
+          {/* Tasty Discounts */}
+          <FeaturedRow id="1234" title="Tasty Discounts" description="Everyone's been enjoying these juicy discounts!" featuredCategory="discounts"/>
+          {/* Offers near you */}
+          <FeaturedRow id="12345" title="Offers near you!" description="Why not support your local restaurant tonight!" featuredCategory="offers"/>
+        </ScrollView>     
     </SafeAreaView>
   );
 }
--- screens/components/Categories.js
+++ screens/components/Categories.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard'
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}>
+      {/* Category card */}
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 1"/>
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 2"/>
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 3"/>      
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 4"/>      
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 5"/>      
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 6"/>      
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 7"/>      
+    </ScrollView>
   )
-}
+} 
